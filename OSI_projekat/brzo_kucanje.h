aeb208c48753274480ac377e14c2098092e6de43
Anja Zjajic <zjajicanja@gmail.com>
2019-01-05 19:27:30 +0100

    Brzo kucanje

---
+++ OSI_projekat/brzo_kucanje.h
@@ -0,0 +1,14 @@
+#ifndef BRZO_KUCANJE_H_INCLUDED
+#define BRZO_KUCANJE_H_INCLUDED
+
+
+void naslovBrzoKucanje();
+void brisiString(char* , char* );
+void unosNetacan(int* , int , int , int );
+void unosTacan(int* , int );
+char* unosStringa(long int *, long int *, char *, int );
+void ispisNivoa(int ,int );
+char generatorStringa();
+int cetvrtaIgra();
+
+#endif // BRZO_KUCANJE_H_INCLUDED
