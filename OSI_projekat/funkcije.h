9f98d9141f380eb3392b748acb3044821a17256c
Anja Zjajic <zjajicanja@gmail.com>
2019-01-05 19:29:18 +0100

    funkcije

---
+++ OSI_projekat/funkcije.h
@@ -0,0 +1,92 @@
+#ifndef IGRICA1_H_INCLUDED
+#define IGRICA1_H_INCLUDED
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+#include <stdio.h>
+#include <time.h>
+#include <windows.h>
+#include <conio.h>
+
+//int pretrazivanje_pokusaja(int niz[], int kljuc);
+
+//int generator();
+
+//int brojac_pokusaja(int niz[]);
+
+//int brojac_bodova(int pokusaj);
+
+//void ispisi();
+
+//int rezim0();
+
+//int rezim1();
+
+//void ispis_bodova(int br_bodova);
+
+//void naslov();
+
+//int igranje();
+
+//int ucitaj_karakter();
+
+//void naslovKviz();
+
+//void ispis(int bodoviDrugaIgra);
+
+//int drugaIgra();
+
+//int pitaj();
+
+//int kontrolaBodova();
+
+//void naslovPlayGround();
+
+//int *unos();
+
+//void okvir();
+
+//int *generisanje_brojeva();
+
+//int izvlacenje(int*,int*);
+
+//void ispis_niza(int*,int);
+
+//void ispis_svega2(int*,int*,int,int,int,int);
+
+//void ispis_svega1(int*,int*,int,int,int);
+
+//void prviIspis(int );
+
+//void zadnjiIspis(int );
+
+//void registracija();
+
+//int otkljucavanje();
+
+//void PlayGround();
+
+//int ispravnostBroja(char *);
+
+//int ispravnostBroja2(char *);
+
+//int meniRegistracija1();
+
+//int meniRegistracija2();
+
+//int meni1();
+
+//void meni2();
+
+//void pamtiBodove(int);
+
+//int ucitajBodove();
+
+
+
+#endif
+
